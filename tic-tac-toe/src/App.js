ba6c53f4c588ab85eb3a5979573f93d7b5346e7b
AshFoster <ashfoster87@gmail.com>
2022-09-02 15:45:45 +0000

    Add board grid

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import GameBoard from './components/GameBoard';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Tic Tac Toe</h1>
+      <button>New Game</button>
+      <div className='game-area'>
+        <GameBoard />
+      </div>
     </div>
   );
 }
---
+++ tic-tac-toe/src/components/GameBoard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './GameBoard.css'
+
+export default function GameBoard() {
+  return (
+    <div className='squares'>
+        <div className='square'></div>
+        <div className='square'></div>
+        <div className='square'></div>
+        <div className='square'></div>
+        <div className='square'></div>
+        <div className='square'></div>
+        <div className='square'></div>
+        <div className='square'></div>
+        <div className='square'></div>
+    </div>
+  )
+}
