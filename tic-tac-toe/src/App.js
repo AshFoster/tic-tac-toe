92866bdef548d2984b9a67f57eeabce4e9c8ad40
AshFoster <ashfoster87@gmail.com>
2022-09-03 09:48:33 +0000

    Add handleClick funtion and useEffect hook

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -3,13 +3,13 @@ import GameBoard from './components/GameBoard';
 
 function App() {
   let numSquares = 9;
-  
+
   return (
     <div className="App">
       <h1>Tic Tac Toe</h1>
       <button>New Game</button>
       <div className='game-area'>
-        <GameBoard numSquares={numSquares}/>
+        <GameBoard numSquares={numSquares} />
       </div>
     </div>
   );
--- tic-tac-toe/src/components/GameBoard.js
+++ tic-tac-toe/src/components/GameBoard.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './GameBoard.css'
 
-export default function GameBoard({ numSquares}) {
-  const squares = [...Array(numSquares)].map(() => Math.random())
+export default function GameBoard({ numSquares }) {
+  const [squares, setSquares] = useState([...Array(numSquares)].map(() => ({ id: Math.random(), selected: false, disabled: false })))
+  const [turns, setTurns] = useState(0)
+  const [selectedSquare, setSelectedSquare] = useState(null)
+
+  const handleClick = (id, disabled) => {
+    if (!disabled) {
+      setTurns(prevTurns => prevTurns + 1)
+      setSelectedSquare(id)
+    }
+  }
+
+  useEffect(() => {
+    if (turns !== 0) {
+      setSquares(prevSquares => {
+        return prevSquares.map(square => {
+          if (square.id === selectedSquare) {
+            return {...square, selected: true, disabled: true, turn: turns}
+          } else {
+            return square
+          }
+        })
+      })
+    }
+  }, [turns, selectedSquare])
+
   return (
     <div className='squares'>
-      {squares.map((id) => (
-        <div className='square' id={id}>
-          <img className='cirle' src='../../img/circle.png' alt='cirle'/>
-          <img className='cross' src='../../img/cross.png' alt='cross'/>
+      {squares.map((square) => (
+        <div className='square' key={square.id} id={square.id} onClick={() => handleClick(square.id, square.disabled)}>
+          <img className={square.selected && square.turn % 2 !== 0 ? 'selected' : ''} src='../../img/circle.png' alt='circle'/>
+          <img className={square.selected && square.turn % 2 === 0 ? 'selected' : ''} src='../../img/cross.png' alt='cross'/>
         </div>
       ))}
     </div>
