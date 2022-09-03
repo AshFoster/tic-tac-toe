bd9987051b3525e7ee8364b758d195ae643e7a8a
AshFoster <ashfoster87@gmail.com>
2022-09-03 11:15:51 +0000

    Add reset game functionality

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import GameBoard from './components/GameBoard';
 
 function App() {
-  let numSquares = 9;
+  const [numSquares, setNumSquares] = useState(9);
+  const [reset, setReset] = useState(false)
+
+  const resetGame = () => {
+    setReset(true)
+  }
 
   return (
     <div className="App">
       <h1>Tic Tac Toe</h1>
-      <button>New Game</button>
+      <button onClick={resetGame}>New Game</button>
       <div className='game-area'>
-        <GameBoard numSquares={numSquares} />
+        <GameBoard numSquares={numSquares} reset={reset} setReset={setReset}/>
       </div>
     </div>
   );
--- tic-tac-toe/src/components/GameBoard.js
+++ tic-tac-toe/src/components/GameBoard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 import './GameBoard.css'
 
-export default function GameBoard({ numSquares }) {
+export default function GameBoard({ numSquares, reset, setReset }) {
   const [squares, setSquares] = useState([...Array(numSquares)].map(() => ({ id: Math.random(), selected: false, disabled: false })))
   const [turns, setTurns] = useState(0)
   const [selectedSquare, setSelectedSquare] = useState(null)
@@ -15,23 +15,30 @@ export default function GameBoard({ numSquares }) {
   }
 
   useEffect(() => {
-    if (turns !== 0) {
-      setSquares(prevSquares => {
-        return prevSquares.map(square => {
-          if (square.id === selectedSquare) {
-            return {...square, selected: true, disabled: true, turn: turns}
-          } else {
-            return square
-          }
+    if (!reset) {
+      if (turns !== 0) {
+        setSquares(prevSquares => {
+          return prevSquares.map(square => {
+            if (square.id === selectedSquare) {
+              return {...square, selected: true, disabled: true, turn: turns}
+            } else {
+              return square
+            }
+          })
         })
-      })
+      }
+    } else {
+      setSquares([...Array(numSquares)].map(() => ({ id: Math.random(), selected: false, disabled: false })))
+      setTurns(0)
+      setSelectedSquare(null)
+      setReset(false)
     }
-  }, [turns, selectedSquare])
+  }, [turns, selectedSquare, reset])
 
   return (
     <div className='squares'>
       {squares.map((square) => (
-        <div className='square' key={square.id} id={square.id} onClick={() => handleClick(square.id, square.disabled)}>
+        <div className={square.selected ? 'square selected' : 'square'} key={square.id} id={square.id} onClick={() => handleClick(square.id, square.disabled)}>
           <img className={square.selected && square.turn % 2 !== 0 ? 'selected' : ''} src='../../img/circle.png' alt='circle'/>
           <img className={square.selected && square.turn % 2 === 0 ? 'selected' : ''} src='../../img/cross.png' alt='cross'/>
         </div>
