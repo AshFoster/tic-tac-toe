d94f287b920b0532b25d257e3abe9d2e69d0e263
AshFoster <ashfoster87@gmail.com>
2022-09-03 08:30:25 +0000

    Refactor code that creates board of squares

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import GameBoard from './components/GameBoard';
 
 function App() {
+  let numSquares = 9;
+  
   return (
     <div className="App">
       <h1>Tic Tac Toe</h1>
       <button>New Game</button>
       <div className='game-area'>
-        <GameBoard />
+        <GameBoard numSquares={numSquares}/>
       </div>
     </div>
   );
--- tic-tac-toe/src/components/GameBoard.js
+++ tic-tac-toe/src/components/GameBoard.js
@@ -2,18 +2,16 @@ import React from 'react'
 
 import './GameBoard.css'
 
-export default function GameBoard() {
+export default function GameBoard({ numSquares}) {
+  const squares = [...Array(numSquares)].map(() => Math.random())
   return (
     <div className='squares'>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
+      {squares.map((id) => (
+        <div className='square' id={id}>
+          <img className='cirle' src='../../img/circle.png' alt='cirle'/>
+          <img className='cross' src='../../img/cross.png' alt='cross'/>
+        </div>
+      ))}
     </div>
   )
 }
